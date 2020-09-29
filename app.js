bca2e6dc589c4c6d0c1f8168a65ff62b4756f4ef
Ramona <drvizitiuramona@gmail.com>
2020-09-29 19:11:54 +0300

    first

---
+++ app.js
@@ -0,0 +1,31 @@
+class Player {
+    constructor(gameContainer) {
+        this.gameContainer = gameContainer;
+    }
+
+    /*
+     Vream sa apara un div care va fi player in gameContainer
+     Pentru inceput, il punem intr-un colt din container
+     Div va avea pozitie absoluta si va ajucati cu x si y
+    */
+    createHtml() {
+
+    }
+
+    // eventul il inregistram pe window/document
+    // verificare de keydown - folositi switch/ if else
+    registerMoving() {}
+
+    movePlayerHtml() {
+
+    }
+
+    // returneaza true sau false
+    checkIfPlayerIsOutsideOfContainer() {
+
+    }
+
+}
+
+const gameContainer = document.querySelector('#gameContainer');
+const player1 = new Player(gameContainer)
